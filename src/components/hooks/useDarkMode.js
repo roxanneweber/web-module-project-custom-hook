d35cbfc2f78f9ddd89fb017653c6cf36464435c3
Roxanne Weber <marie.sankofa@gmail.com>
2021-12-03 02:05:34 -0600

    useDarkMode created and imported into App

---
+++ src/components/hooks/useDarkMode.js
@@ -0,0 +1,7 @@
+import { useState } from 'react';
+
+const useDarkMode = (initialValue) => {
+	const [darkMode, setDarkMode] = useState(initialValue);
+	return [darkMode, setDarkMode];
+};
+export default useDarkMode;
